67e3ee18b8b0e79049629dc9b0b00ccbf3dba462
Don Onwunumah <don.onwunumah@10xbanking.com>
2018-04-20 16:24:02 +0100

    Wrapped SectionList for use in Typescript removing propstypes

--- src/section-list/index.js
+++ src/section-list/index.js
@@ -1,20 +1,7 @@
 import React, { Component } from 'react';
 import { SectionList } from 'react-native';
-import PropTypes from 'prop-types';
 
 class TenxWrappedSectionList extends Component {
-  static propTypes = {
-    style: PropTypes.any,
-    onRefresh: PropTypes.any,
-    sections: PropTypes.any,
-    renderItem: PropTypes.any,
-    refreshing: PropTypes.any,
-    renderSectionHeader: PropTypes.any,
-    keyExtractor: PropTypes.any,
-    scrollEnabled: PropTypes.any,
-    onViewableItemsChanged: PropTypes.any
-  };
-
   constructor(props) {
     super(props);
   }
@@ -22,15 +9,7 @@ class TenxWrappedSectionList extends Component {
   render() {
     return (
         <SectionList
-            style={this.props.style}
-            onRefresh={this.props.onRefresh}
-            sections={this.props.sections}
-            renderItem={this.props.renderItem}
-            refreshing={this.props.isRefreshing}
-            renderSectionHeader={this.props.renderSectionHeader}
-            keyExtractor={this.props.keyExtractor}
-            scrollEnabled={this.props.scrollEnabled}
-            onViewableItemsChanged={this.props.onViewableItemsChanged}
+			{...this.props}
         />
     );
   }
